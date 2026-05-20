798a3fcdaae6f3d83917e18bbe4118f53c0f6550
Nymuxyzo <1729839+Nymuxyzo@users.noreply.github.com>
2026-05-20 15:15:30 +0200

    Add `total_amount`, `total_amount_in_cents`, and `currency` methods to BatchDetail

--- lib/sepa_file_parser/general/batch_detail.rb
+++ lib/sepa_file_parser/general/batch_detail.rb
@@ -7,6 +7,20 @@ module SepaFileParser
 
     def initialize(xml_data)
       @xml_data = xml_data
+      @amount = xml_data.xpath('TtlAmt/text()').text
+    end
+
+    def total_amount
+      SepaFileParser::Misc.to_amount(@amount)
+    end
+
+    def total_amount_in_cents
+      SepaFileParser::Misc.to_amount_in_cents(@amount)
+    end
+
+    # @return [String]
+    def currency
+      @currency ||= xml_data.xpath('TtlAmt/@Ccy').text
     end
 
     def payment_information_identification
@@ -16,6 +30,7 @@ module SepaFileParser
     def msg_id # may be missing
       @msg_id ||= xml_data.xpath('MsgId/text()').text
     end
+    alias_method :message_id, :msg_id # same like in transaction.rb
 
     def number_of_transactions
       @number_of_transactions ||= xml_data.xpath('NbOfTxs/text()').text
