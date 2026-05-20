8853baf61cf673ca1b86b244eefc0078e6216380
Nymuxyzo <1729839+Nymuxyzo@users.noreply.github.com>
2026-05-20 15:15:30 +0200

    Add `total_amount`, `total_amount_in_cents`, and `currency` methods to BatchDetail

--- spec/lib/sepa_file_parser/general/batch_detail_spec.rb
+++ spec/lib/sepa_file_parser/general/batch_detail_spec.rb
@@ -3,15 +3,31 @@
 require 'spec_helper'
 
 RSpec.describe SepaFileParser::BatchDetail do
-  let(:camt)       { SepaFileParser::File.parse('spec/fixtures/camt053/valid_example_with_batch.xml') }
-  let(:statements)     { camt.statements }
-  let(:ex_stmt)        { statements[0] }
-  let(:entries)        { ex_stmt.entries }
-  let(:ex_entry)       { entries[0] }
-  let(:batch_detail)   { ex_entry.batch_detail }
+  context 'camt052' do
+    let(:camt)          { SepaFileParser::File.parse('spec/fixtures/camt052/valid_example_with_batch.xml') }
+    let(:reports)       { camt.reports }
+    let(:ex_rpt)        { reports[0] }
+    let(:entries)       { ex_rpt.entries }
+    let(:ex_entry)      { entries[0] }
+    let(:batch_detail)  { ex_entry.batch_detail }
 
-  specify { expect(batch_detail.payment_information_identification).to eq("O0OpeAYTkhjerKu3eE9asw") }
-  specify { expect(batch_detail.number_of_transactions).to eq("3") }
-  specify { expect(batch_detail.msg_id).to eq('02453b1e17c11241073a777ad9c273b4149') }
-  specify { expect(batch_detail.xml_data).to_not be_nil }
+    specify { expect(batch_detail.total_amount).to eq BigDecimal("1500") }
+    specify { expect(batch_detail.total_amount_in_cents).to eq(150000) }
+    specify { expect(batch_detail.currency).to eq("EUR") }
+    specify { expect(batch_detail.xml_data).to_not be_nil }
+  end
+
+  context 'camt053' do
+    let(:camt)       { SepaFileParser::File.parse('spec/fixtures/camt053/valid_example_with_batch.xml') }
+    let(:statements)     { camt.statements }
+    let(:ex_stmt)        { statements[0] }
+    let(:entries)        { ex_stmt.entries }
+    let(:ex_entry)       { entries[0] }
+    let(:batch_detail)   { ex_entry.batch_detail }
+
+    specify { expect(batch_detail.payment_information_identification).to eq("O0OpeAYTkhjerKu3eE9asw") }
+    specify { expect(batch_detail.number_of_transactions).to eq("3") }
+    specify { expect(batch_detail.msg_id).to eq('02453b1e17c11241073a777ad9c273b4149') }
+    specify { expect(batch_detail.xml_data).to_not be_nil }
+  end
 end
